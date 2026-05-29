6bc51e25749a5df2cb28e0251116964d02dcb4fe
krittika1208 <krittika1208@gmail.com>
2026-05-29 23:52:54 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,27 +1,22 @@
-{
-"expo": {
-"name": "Shelf Therapy",
-"slug": "shelf-therapy",
-"version": "1.0.0",
-"orientation": "portrait",
-"userInterfaceStyle": "light",
-"splash": {
-"resizeMode": "contain",
-"backgroundColor": "#FFF8F7"
-},
-"assetBundlePatterns": [
-"**/*"
-],
-"ios": {
-"supportsTablet": true
-},
-"android": {
-"adaptiveIcon": {
-"backgroundColor": "#FFF8F7"
-}
-},
-"web": {
-"bundler": "metro"
-}
-}
-}
+import React, { useEffect, useState } from 'react';
+import {
+View,
+Text,
+StyleSheet,
+ScrollView,
+TouchableOpacity,
+SafeAreaView,
+TextInput,
+Modal,
+Alert
+} from 'react-native';
+
+import { db } from './firebase';
+
+import {
+collection,
+addDoc,
+getDocs,
+deleteDoc,
+doc
+} from 'firebase/firestore';
